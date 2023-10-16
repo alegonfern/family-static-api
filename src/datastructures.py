c9fc7992666dfa91f001a93eda8f7aa415124df3
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-10-16 06:33:53 +0000

    act1 error members

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,4 +1,3 @@
-
 """
 update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
@@ -11,26 +10,51 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
-        # example list of members
         self._members = []
+        self.add_member({
+            "first_name": "John",
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]
+        })
+        self.add_member({
+            "first_name": "Jane",
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]
+        })
+        self.add_member({
+            "first_name": "Jimmy",
+            "age": 5,
+            "lucky_numbers": [1]
+        })
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
+    def get_all_members(self):
+        return self._members
+
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        if "id" not in member:
+            member["id"] = self._generateId()
+
+        self._members.append(member)
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member.get("id") == id:
+                self._members.remove(member)
+                return True
 
-    def get_member(self, id):
-        # fill this method and update the return
-        pass
+        return False
 
-    # this method is done, it returns a list with all the family members
-    def get_all_members(self):
-        return self._members
+    def get_member(self, member_id):
+        for member in self._members:
+            if member["id"] == member_id:
+                return {
+                    "id": member["id"],
+                    "first_name": member["first_name"],
+                    "age": member["age"],
+                    "lucky_numbers": member["lucky_numbers"]
+                }
+
+        return None
